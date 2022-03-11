6278749417dae620fcc4d5310433ea476d57c05c
Mehedi-Hasan-Shakil <80805146+Mehedi-Hasan-Shakil@users.noreply.github.com>
2022-03-11 16:36:08 +0600

    Main class

--- ComboLock/ComboLockTester.java
+++ ComboLock/ComboLockTester.java
@@ -1,20 +1,10 @@
-import java.util.Scanner;
-
-class ComboLockTester {
+public class ComboLockTester {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int turnRight1 = in.nextInt();
-        int turnLeft = in.nextInt();
-        int turnRight2 = in.nextInt();
         ComboLock lock = new ComboLock(36, 24, 36);
         lock.reset();
-        lock.turnRight(turnRight1);
-        lock.turnLeft(turnLeft);
-        lock.turnRight(turnRight2);
-        if (lock.open()) {
-            System.out.println("Open");
-        } else {
-            System.out.println("Wrong combination.");
-        }
+        lock.turnRight(36);
+        lock.turnLeft(12);
+        lock.turnRight(11);
+        lock.open();
     }
 }
