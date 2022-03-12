e58f06b11dc31cd1df9157d7d652b372b27b6c94
Mehedi-Hasan-Shakil <80805146+Mehedi-Hasan-Shakil@users.noreply.github.com>
2022-03-12 10:43:17 +0600

    Floating point number inputs

---
+++ E11.13/NumberInputs.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+/*Write a program that asks the user to input a set of floating-point values. When the
+  user enters a value that is not a number, give the user a second chance to enter the
+  value. After two chances, quit reading input. Add all correctly specified values and
+  print the sum when the user is done entering data. Use exception handling to detect
+  improper inputs.
+ */
+
+public class Total {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int chances = 3;
+        double sum = 0;
+        System.out.println("Enter numbers : ");
+        while (chances > 0) {
+            try {
+                sum = sum + in.nextDouble();
+            } catch (InputMismatchException e) {
+                chances--;
+                if (!in.hasNextDouble()) {
+                    System.out.println("You have " + chances + " chances left");
+                    in.next();
+                }
+            }
+        }
+        System.out.println("Sum = " + sum);
+        in.close();
+    }
+}
