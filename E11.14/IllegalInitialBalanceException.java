950fa55c3e8d31c30a80b238e243d073dc468e8e
Mehedi-Hasan-Shakil <80805146+Mehedi-Hasan-Shakil@users.noreply.github.com>
2022-03-12 11:30:31 +0600

    Exception class

---
+++ E11.14/IllegalInitialBalanceException.java
@@ -0,0 +1,6 @@
+public class IllegalInitialBalanceException extends IllegalArgumentException{
+    public IllegalInitialBalanceException() {}
+    public IllegalInitialBalanceException(String message) {
+        super(message);
+    }
+}
