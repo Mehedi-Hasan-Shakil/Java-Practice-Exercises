1ecb01e625f8e9d1d79328e651eaf1ba7e05c87e
Mehedi-Hasan-Shakil <80805146+Mehedi-Hasan-Shakil@users.noreply.github.com>
2022-03-14 00:02:22 +0600

    Main class

--- BankAccountWithNumber/BankAccountTester.java
+++ BankAccountWithNumber/BankAccountTester.java
@@ -1,27 +1,12 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BankAccountTester {
     public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<BankAccount> accounts = readFile();
-        BankAccount largestAccount = accounts.get(0);
-        for (BankAccount account : accounts) {
-            if (account.getBalance() > largestAccount.getBalance()) {
-                largestAccount = account;
-            }
-        }
-        System.out.println("Account : " + largestAccount.getAccountNumber());
-        System.out.println("Balance : " + largestAccount.getBalance());
-    }
-
-    static ArrayList<BankAccount> readFile() throws FileNotFoundException {
-        Scanner in = new Scanner(new File("input.txt"));
-        ArrayList<BankAccount> accounts = new ArrayList<>();
-        while (in.hasNextLine()) {
-            accounts.add(new BankAccount(in.nextInt(), in.nextDouble()));
-        }
-        return accounts;
+        Scanner in = new Scanner(System.in);
+        Bank bank = new Bank();
+        ArrayList<BankAccount> accounts = bank.readFile(in.next());
+        System.out.println("Largest account balance is : " + bank.largestAccount(accounts).getBalance());
     }
 }
