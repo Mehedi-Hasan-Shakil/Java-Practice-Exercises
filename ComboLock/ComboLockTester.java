ae830f33273239b7464ec19b9a7327844ead1701
Mehedi-Hasan-Shakil <80805146+Mehedi-Hasan-Shakil@users.noreply.github.com>
2022-03-14 23:17:00 +0600

    Main class

--- ComboLock/ComboLockTester.java
+++ ComboLock/ComboLockTester.java
@@ -1,10 +1,13 @@
 public class ComboLockTester {
     public static void main(String[] args) {
-        ComboLock lock = new ComboLock(36, 24, 36);
+        ComboLock lock = new ComboLock(7, 6, 5);
         lock.reset();
-        lock.turnRight(36);
-        lock.turnLeft(12);
-        lock.turnRight(11);
-        lock.open();
+        lock.turnRight(7);
+        lock.turnLeft(1);
+        lock.turnRight(39);
+        if (lock.open())
+            System.out.println("Opened");
+        else
+            System.out.println("Wrong combination");
     }
 }
